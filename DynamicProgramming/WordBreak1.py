f367cf783c6886e3a36bfbbef142dd7a561c713c
Julien Mihai <mihaijulien@gmail.com>
2024-04-23 20:02:57 +0300

    dp + 1

---
+++ DynamicProgramming/WordBreak1.py
@@ -0,0 +1,22 @@
+'''
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+
+                    break
+        
+        return dp[-1]
